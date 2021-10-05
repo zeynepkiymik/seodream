c39f4a275e51ca127ad18b3e33b5807d9f59434f
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-10-05 14:39:37 +0300

    Create Kisiler.php

---
+++ app/Models/Kisiler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kisiler extends Model
+{
+    use HasFactory;
+    protected $table = 'kisiler';
+    public $timestamps = false;
+}
